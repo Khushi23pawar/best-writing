269b44a34787447e3a444e321cc6ef27d4c2ef12
Khushi Pawar <khushpawar27@gmail.com>
2024-02-15 15:33:56 +0530

    my first commit

---
+++ js/custom.js
@@ -0,0 +1,335 @@
+var arrayofobj = [{
+    logo: "images/simply-business.jpg",
+    heading: "News Writer",
+    typeofjob: "Warren County Record",
+    timing: "Full-time,Support",
+    location: "United Kingdom",
+    applylink: "job.html",
+    joblink: "job.html",
+    posttime: "1 Hour",
+}, {
+    logo: "images/siege-media.jpg",
+    heading: "Media Player",
+    typeofjob: "Warren County Record",
+    timing: "Part-time",
+    location: "India",
+    applylink: "job.html",
+    joblink: "job.html",
+    posttime: "1 Hour",
+
+
+}, {
+    logo: "images/simply-business.jpg",
+    heading: "News Writer",
+    typeofjob: "Warren County Record",
+    timing: "Full-time,Support",
+    location: "United Kingdom",
+    applylink: "job.html",
+    joblink: "job.html",
+    posttime: "1 Hour",
+
+
+}, {
+    logo: "images/ted.jpg",
+    heading: "Ted Talk",
+    typeofjob: "Warren County Record",
+    timing: "Full-time,Support",
+    location: "Mumbai",
+    applylink: "job.html",
+    joblink: "job.html",
+    posttime: "1 Hour",
+
+
+}, {
+    logo: "images/semrush.png",
+    heading: "Semrush",
+    typeofjob: "Warren County Record",
+    timing: "Full-time,Support",
+    location: "London",
+    applylink: "job.html",
+    joblink: "job.html",
+    posttime: "1 Hour",
+
+
+}, {
+    logo: "images/wix.png",
+    heading: "WIX",
+    typeofjob: "Warren County Record",
+    timing: "Full-time,Support",
+    location: "Delhi",
+    applylink: "job.html",
+    joblink: "job.html",
+    posttime: "1 Hour",
+}]
+var companyobj = [{
+        logo: "images/draft-dev.png",
+        heading: "Draft.Dev",
+        text: "23 jobs posted"
+    }, {
+        logo: "images/wix.png",
+        heading: "Wix",
+        text: "20 jobs posted"
+    }, {
+        logo: "images/zapier.jpg",
+        heading: "Zapier",
+        text: "19 jobs posted"
+    }, {
+        logo: "images/university-of-austin.jpg",
+        heading: "Aus.Uni",
+        text: "40 jobs posted"
+    }, {
+        logo: "images/semrush.png",
+        heading: "Semrush",
+        text: "26 jobs posted"
+    }, {
+        logo: "images/robinhood.jpg",
+        heading: "Robinhood",
+        text: "16 jobs posted"
+    }, {
+        logo: "images/nerdwallet.jpg",
+        heading: "Nerdwallet",
+        text: "20 jobs posted"
+    }, {
+        logo: "images/floatcom.jpg",
+        heading: "Float.Com",
+        text: "14 jobs posted"
+    }, {
+        logo: "images/ted.jpg",
+        heading: "Ted Talk",
+        text: "10 jobs posted"
+    }, {
+        logo: "images/studycom.png",
+        heading: "Study.Com",
+        text: "25 jobs posted"
+    }, {
+        logo: "images/shoutvox.jpg",
+        heading: "Shoutvox",
+        text: "23 jobs posted"
+    }, {
+        logo: "images/reddit.png",
+        heading: "Reddit",
+        text: "16 jobs posted"
+    }, {
+        logo: "images/forbes.png",
+        heading: "Forbes",
+        text: "15 jobs posted"
+    }, {
+        logo: "images/kraken.jpg",
+        heading: "Kraken",
+        text: "20 jobs posted"
+    }, {
+        logo: "images/insurify.jpg",
+        heading: "Insurify",
+        text: "19 jobs posted"
+    }, {
+        logo: "images/figma.jpg",
+        heading: "Figma",
+        text: "30 jobs posted"
+    }, {
+        logo: "images/aha.png",
+        heading: "AHA!",
+        text: "26 jobs posted"
+    }, {
+        logo: "images/calm.png",
+        heading: "Calm",
+        text: "7 jobs posted"
+    }, {
+        logo: "images/launch-potato.png",
+        heading: "Launch Potato",
+        text: "5 jobs posted"
+    }, {
+        logo: "images/animalz.png",
+        heading: "Amimalz",
+        text: "20 jobs posted"
+    }, {
+        logo: "images/clickup.png",
+        heading: "Clickup",
+        text: "19 jobs posted"
+    }, {
+        logo: "images/evenup.jpg",
+        heading: "Evenup",
+        text: "28 jobs posted"
+    }, {
+        logo: "images/raketech.jpg",
+        heading: "Raketch",
+        text: "24 jobs posted"
+    }, {
+        logo: "images/the-hoth.jpg",
+        heading: "The Hoth",
+        text: "16 jobs posted"
+    },
+
+]
+var writerdetail_obj = [{
+        image: "images/f132f79f-e605-4d7b-af85-22495e6e4060.jpg",
+        name: "Khushi Pawar",
+        work: "Seo specialist"
+    }, {
+        image: "images/4fb1acd1-ef39-4f02-ad17-a2cf1c351f8f.jpg",
+        name: "Khushboo Pawar",
+        work: "Keyword Rsearch"
+    }, {
+        image: "images/0ae6c4fd-9e4c-41a6-9bd8-978bd0e119d6.jpg",
+        name: "Yashasvi Sharma",
+        work: "UI-UX Design"
+    }, {
+        image: "images/0c186951-2e8f-4dc2-b0d0-b59c3a79e290.png",
+        name: "Mayur Pujari",
+        work: "Business Man"
+    }, {
+        image: "images/462d8dd6-534c-4fed-a782-bb4ef9ccef07.jpg",
+        name: "Aachal Shah",
+        work: "Content Writer"
+    }, {
+        image: "images/2a2dfe8f-45ae-4429-881d-a73d86eef569.jpg",
+        name: "Harsh bagul",
+        work: "Blogger"
+    },{
+        image: "images/f132f79f-e605-4d7b-af85-22495e6e4060.jpg",
+        name: "Khushi Pawar",
+        work: "Seo specialist"
+    }, {
+        image: "images/4fb1acd1-ef39-4f02-ad17-a2cf1c351f8f.jpg",
+        name: "Khushboo Pawar",
+        work: "Keyword Rsearch"
+    }, {
+        image: "images/0ae6c4fd-9e4c-41a6-9bd8-978bd0e119d6.jpg",
+        name: "Yashasvi Sharma",
+        work: "UI-UX Design"
+    }, {
+        image: "images/0c186951-2e8f-4dc2-b0d0-b59c3a79e290.png",
+        name: "Mayur Pujari",
+        work: "Business Man"
+    }, {
+        image: "images/462d8dd6-534c-4fed-a782-bb4ef9ccef07.jpg",
+        name: "Aachal Shah",
+        work: "Content Writer"
+    }, {
+        image: "images/2a2dfe8f-45ae-4429-881d-a73d86eef569.jpg",
+        name: "Harsh bagul",
+        work: "Blogger"
+    },{
+        image: "images/f132f79f-e605-4d7b-af85-22495e6e4060.jpg",
+        name: "Khushi Pawar",
+        work: "Seo specialist"
+    }, {
+        image: "images/4fb1acd1-ef39-4f02-ad17-a2cf1c351f8f.jpg",
+        name: "Khushboo Pawar",
+        work: "Keyword Rsearch"
+    }, {
+        image: "images/0ae6c4fd-9e4c-41a6-9bd8-978bd0e119d6.jpg",
+        name: "Yashasvi Sharma",
+        work: "UI-UX Design"
+    }, {
+        image: "images/0c186951-2e8f-4dc2-b0d0-b59c3a79e290.png",
+        name: "Mayur Pujari",
+        work: "Business Man"
+    }, {
+        image: "images/462d8dd6-534c-4fed-a782-bb4ef9ccef07.jpg",
+        name: "Aachal Shah",
+        work: "Content Writer"
+    }, {
+        image: "images/2a2dfe8f-45ae-4429-881d-a73d86eef569.jpg",
+        name: "Harsh bagul",
+        work: "Blogger"
+    },{
+        image: "images/f132f79f-e605-4d7b-af85-22495e6e4060.jpg",
+        name: "Khushi Pawar",
+        work: "Seo specialist"
+    }, {
+        image: "images/4fb1acd1-ef39-4f02-ad17-a2cf1c351f8f.jpg",
+        name: "Khushboo Pawar",
+        work: "Keyword Rsearch"
+    }, {
+        image: "images/0ae6c4fd-9e4c-41a6-9bd8-978bd0e119d6.jpg",
+        name: "Yashasvi Sharma",
+        work: "UI-UX Design"
+    }, {
+        image: "images/0c186951-2e8f-4dc2-b0d0-b59c3a79e290.png",
+        name: "Mayur Pujari",
+        work: "Business Man"
+    }, {
+        image: "images/462d8dd6-534c-4fed-a782-bb4ef9ccef07.jpg",
+        name: "Aachal Shah",
+        work: "Content Writer"
+    }, {
+        image: "images/2a2dfe8f-45ae-4429-881d-a73d86eef569.jpg",
+        name: "Harsh bagul",
+        work: "Blogger"
+    },{
+        image: "images/f132f79f-e605-4d7b-af85-22495e6e4060.jpg",
+        name: "Khushi Pawar",
+        work: "Seo specialist"
+    }, {
+        image: "images/4fb1acd1-ef39-4f02-ad17-a2cf1c351f8f.jpg",
+        name: "Khushboo Pawar",
+        work: "Keyword Rsearch"
+    }, {
+        image: "images/0ae6c4fd-9e4c-41a6-9bd8-978bd0e119d6.jpg",
+        name: "Yashasvi Sharma",
+        work: "UI-UX Design"
+    }, {
+        image: "images/0c186951-2e8f-4dc2-b0d0-b59c3a79e290.png",
+        name: "Mayur Pujari",
+        work: "Business Man"
+    }, {
+        image: "images/462d8dd6-534c-4fed-a782-bb4ef9ccef07.jpg",
+        name: "Aachal Shah",
+        work: "Content Writer"
+    }, {
+        image: "images/2a2dfe8f-45ae-4429-881d-a73d86eef569.jpg",
+        name: "Harsh bagul",
+        work: "Blogger"
+    },{
+        image: "images/f132f79f-e605-4d7b-af85-22495e6e4060.jpg",
+        name: "Khushi Pawar",
+        work: "Seo specialist"
+    }, {
+        image: "images/4fb1acd1-ef39-4f02-ad17-a2cf1c351f8f.jpg",
+        name: "Khushboo Pawar",
+        work: "Keyword Rsearch"
+    }, {
+        image: "images/0ae6c4fd-9e4c-41a6-9bd8-978bd0e119d6.jpg",
+        name: "Yashasvi Sharma",
+        work: "UI-UX Design"
+    }, {
+        image: "images/0c186951-2e8f-4dc2-b0d0-b59c3a79e290.png",
+        name: "Mayur Pujari",
+        work: "Business Man"
+    }, {
+        image: "images/462d8dd6-534c-4fed-a782-bb4ef9ccef07.jpg",
+        name: "Aachal Shah",
+        work: "Content Writer"
+    }, {
+        image: "images/2a2dfe8f-45ae-4429-881d-a73d86eef569.jpg",
+        name: "Harsh bagul",
+        work: "Blogger"
+    }
+
+]
+
+
+
+$(document).ready(function() {
+	var targetTotemplate = [{
+		target: "job-template-box",
+		template:"job-template",
+		data:arrayofobj,
+	},{
+		target: "company-logo-template",
+		template:"company-logo-inner-template",
+		data:companyobj,
+	},{
+		target: "writer-template",
+		template:"writer-template-circle",
+		data:writerdetail_obj,
+	}]
+
+	for(var i=0;i<targetTotemplate.length;i++){
+	    target = $("#"+targetTotemplate[i].target)
+	    template = $("#"+targetTotemplate[i].template).html();
+	    var html = Mustache.render(template, { arr: targetTotemplate[i].data });
+	    target.append(html);
+	}
+
+});
